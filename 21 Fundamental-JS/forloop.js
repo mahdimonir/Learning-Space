23aceec5a2ae23bf4d17d8c356df5e8865bf3ef9
mahdimonir <mahdimoniruzzaman@gmail.com>
2025-07-07 21:45:36 +0600

    Add JavaScript practice files covering arrays, conditionals, loops, functions, objects, switch statements, and variable manipulations. Each file demonstrates fundamental concepts with examples and console outputs for clarity.

---
+++ 21 Fundamental-JS/forloop.js
@@ -0,0 +1,15 @@
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+    
+}
+
+for (var i = 20; i <= 25; i++) {
+    console.log(i);
+    
+}
+
+var num = [55, 66, 77, 88, 99, 11, 44];
+for (var i = 0; i< num.length; i++){
+    var element = num[i];
+    console.log(element);
+}
---
+++ 21 Fundamental-JS/while.js
@@ -0,0 +1,11 @@
+var num = 0;
+while (num < 15) {
+    console.log(num);
+    num = num + 1;  
+}
+
+var num = 20;
+while (num <= 25) {
+    console.log(num);
+    num = num + 1;  
+}
