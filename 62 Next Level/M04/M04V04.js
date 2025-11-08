9a61e875f7505b2bd2770fd98d9bdd5d65181a87
mahdimonir <mahdimoniruzzaman@gmail.com>
2025-11-08 18:48:59 +0600

    M04V04: Implement twoSum function to find indices of two numbers that add up to target

---
+++ 62 Next Level/M04/M04V04.js
@@ -0,0 +1,34 @@
+//? Problem Statement
+
+//* Given an array of integers numbers and an integer target, return the indixs of two numbers such that they add up to target.
+//* You may assume that each input would have exactly one solution, and you may not use the same element twice
+//* If there is no solution then retunr undefined
+
+//* Time Complexity => O(n)
+
+//? Input: [2, 11, 7, 15] and 9
+
+//? Output: [0, 2] (because 2 + 7 = 9)
+
+const twoSum = (arr, target) => {
+  const numbMap = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    const currentNumber = arr[i];
+    const complement = target - currentNumber;
+
+    console.log("currentNumber", currentNumber);
+    console.log("complement", complement);
+
+    if (numbMap.has(complement)) {
+      return [numbMap.get(complement), i];
+    }
+
+    numbMap.set(currentNumber, i);
+
+    console.log(numbMap);
+  }
+  return undefined;
+};
+
+console.log(twoSum([2, 11, 20, 7, 15], 9));
