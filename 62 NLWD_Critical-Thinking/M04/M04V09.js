293f810c6e4a3f0a256ac1169c9e6ed995b5ae15
mahdimonir <mahdimoniruzzaman@gmail.com>
2025-11-09 10:53:21 +0600

    Fix insertionSort index error in loop initialization

--- 62 NLWD_Critical-Thinking/M04/M04V09.js
+++ 62 NLWD_Critical-Thinking/M04/M04V09.js
@@ -3,7 +3,7 @@
 const insertionSort = (arr) => {
   for (let i = 1; i < arr.length; i++) {
     let current = arr[i];
-    let j = i + 1;
+    let j = i - 1;
     while (j >= 0 && arr[j] >= current) {
       arr[j + 1] = arr[j];
       j--;
