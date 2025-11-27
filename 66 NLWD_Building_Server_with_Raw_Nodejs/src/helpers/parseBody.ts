2c22b1b9a1d051148c8c638292ff5b9f4de2489b
mahdimonir <mahdimoniruzzaman@gmail.com>
2025-11-27 23:50:47 +0600

    Add parse body to parse data

---
+++ 66 NLWD_Building_Server_with_Raw_Nodejs/src/helpers/parseBody.ts
@@ -0,0 +1,19 @@
+import { IncomingMessage } from "http";
+
+export default async function parseBody(req: IncomingMessage): Promise<any> {
+  return new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      try {
+        resolve(body ? JSON.parse(body) : {});
+      } catch (error: any) {
+        reject(error);
+      }
+    });
+
+    req.on("error", reject);
+  });
+}
