eac20d6f15351c8b4acca2e546244b6d82002301
mahdimonir <mahdimoniruzzaman@gmail.com>
2025-11-27 23:49:46 +0600

    Add routes for centrally handle routes

---
+++ 66 NLWD_Building_Server_with_Raw_Nodejs/src/routes/index.ts
@@ -0,0 +1,63 @@
+import { readUsers, writeUsers } from "../helpers/fileDB";
+import parseBody from "../helpers/parseBody";
+import addRoutes from "../helpers/RouteHandler";
+import sendJson from "../helpers/sendJson";
+
+addRoutes("GET", "/", (req, res) => {
+  sendJson(res, 200, {
+    message: "Hello from node js with typescript...",
+    path: req.url,
+  });
+});
+
+addRoutes("GET", "/api", (req, res) => {
+  sendJson(res, 200, {
+    message: "Health status ok...",
+    path: req.url,
+  });
+});
+
+addRoutes("POST", "/api/users", async (req, res) => {
+  const body = await parseBody(req);
+
+  const users = readUsers();
+
+  const newUser = {
+    id: Date.now(),
+    ...body,
+  };
+  users?.push(newUser);
+
+  writeUsers(users);
+
+  sendJson(res, 201, { success: true, data: newUser });
+});
+
+addRoutes("PUT", "/api/users/:id", async (req, res) => {
+  const { id } = (req as any).params;
+  const body = await parseBody(req);
+
+  const users = readUsers();
+
+  const index = users.findIndex((user: any) => user.id == id);
+
+  if (index === -1) {
+    sendJson(res, 404, {
+      success: false,
+      message: "user not found",
+    });
+  }
+
+  users[index] = {
+    ...users[index],
+    ...body,
+  };
+
+  writeUsers(users);
+
+  sendJson(res, 202, {
+    success: true,
+    message: `Updated user info for id ${id}`,
+    data: users[index],
+  });
+});
