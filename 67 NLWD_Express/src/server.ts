7df916b486c8af939701cc89c04e8e89bf97efae
mahdimonir <mahdimoniruzzaman@gmail.com>
2025-11-30 23:32:00 +0600

    Add server setup with DB

---
+++ 67 NLWD_Express/src/server.ts
@@ -0,0 +1,67 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import path from "path";
+import { Pool } from "pg";
+
+dotenv.config({
+  path: path.join(process.cwd(), ".env"),
+});
+const app = express();
+const port = 8000;
+
+// parser
+app.use(express.json());
+// app.use(express.urlencoded())
+
+// DB
+const pool = new Pool({
+  connectionString: `${process.env.CONNECTION_STR}`,
+});
+
+const initDB = async () => {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS todos(
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(id) ON DELETE CASCADE,
+        title VARCHAR(200) NOT NULL,
+        description TEXT,
+        completed BOOLEAN DEFAULT false,
+        due_date DATE,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+};
+initDB();
+
+// routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Next Level Web Developer!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+    data: req.body,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listenign on port ${port}`);
+});
