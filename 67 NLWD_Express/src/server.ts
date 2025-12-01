9805a96e36f4323ea170024c15c8f36f3b18de4b
mahdimonir <mahdimoniruzzaman@gmail.com>
2025-12-01 23:09:14 +0600

    Add create todo route

--- 67 NLWD_Express/src/server.ts
+++ 67 NLWD_Express/src/server.ts
@@ -180,6 +180,30 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
   }
 });
 
+// create todo
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,
+      [user_id, title]
+    );
+    // console.log(result.rows[0]);
+
+    res.status(201).json({
+      success: true,
+      message: "Todo Created successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listenign on port ${port}`);
 });
